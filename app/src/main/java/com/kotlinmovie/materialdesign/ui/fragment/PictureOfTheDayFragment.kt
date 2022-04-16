908508006e498b06bee4d22f6c0f405aed9c4c18
Dmitrii <dim1928@yandex.ru>
2022-04-17 02:31:01 +0300

    HomeWork5. Работа с constraintSet. Во фрагменте PictureOfTheDayFragment Выскакивает Textview с вверху вниз при клике чипа. При повтрном клике чипа прячем Textview и снова показываем сменив дату.

--- app/src/main/java/com/kotlinmovie/materialdesign/ui/fragment/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/kotlinmovie/materialdesign/ui/fragment/PictureOfTheDayFragment.kt
@@ -4,16 +4,16 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import androidx.transition.Fade
-import androidx.transition.TransitionManager
-import androidx.transition.TransitionSet
 import android.view.*
+import android.view.animation.AnticipateOvershootInterpolator
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.transition.*
 import coil.api.load
 import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -22,6 +22,8 @@ import com.kotlinmovie.materialdesign.databinding.FragmentMainPictureOfDayBindin
 import com.kotlinmovie.materialdesign.ui.MainActivity
 import com.kotlinmovie.materialdesign.viewModel.PictureOfTheDayState
 import com.kotlinmovie.materialdesign.viewModel.PictureOfTheDayViewModel
+import kotlinx.coroutines.NonDisposableHandle.parent
+import org.chromium.base.ThreadUtils.runOnUiThread
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
@@ -48,6 +50,8 @@ class PictureOfTheDayFragment : Fragment() {
 
     private var mContext: Context? = null
     private var selectedDate = "2022-02-21"
+
+    private var durationSet = 1000L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -97,38 +101,67 @@ class PictureOfTheDayFragment : Fragment() {
         binding.chipGroup.setOnCheckedChangeListener { group, checkedId ->
             when (checkedId) {
                 R.id.today_chip1 -> {
-                    disappearPictureOaTheDay()
-                    viewModel.sendServerRequest(selectedDate,onError = ::loadingError)
+
+                    constraintSetAnimationStart()
+                    disappearPictureOfTheDay()
+                    viewModel.sendServerRequest(selectedDate, onError = ::loadingError)
                     binding.imageView.visibility = View.INVISIBLE
-                binding.dataTextview.text = "Дата картинки "+ selectedDate}
+                    binding.dataTextview.text = "Дата картинки " + selectedDate
+                   Thread {
+                       Thread.sleep(durationSet )
+                       runOnUiThread {
+                           binding.TextviewConstraintSet.text = selectedDate
+                       }
+                   }.start()
+                }
                 R.id.yestrday_chip2 -> {
-                    disappearPictureOaTheDay()
-                    viewModel.sendServerRequest(takeDate(-1),onError = ::loadingError)
-                    binding.dataTextview.text = "Дата картинки "+ takeDate(-1)
+
+                    constraintSetAnimationStart()
+                    disappearPictureOfTheDay()
+                    viewModel.sendServerRequest(takeDate(-1), onError = ::loadingError)
+                    binding.dataTextview.text = "Дата картинки " + takeDate(-1)
+                    Thread {
+                        Thread.sleep(durationSet )
+                        runOnUiThread {
+                            binding.TextviewConstraintSet.text = takeDate(-1)
+                        }
+                    }.start()
+
                 }
                 R.id.befoYestrday_chip3 -> {
-                    disappearPictureOaTheDay()
-                    viewModel.sendServerRequest(takeDate(-2),onError = ::loadingError)
-                    binding.dataTextview.text = "Дата картинки "+ takeDate(-2)
+
+                    constraintSetAnimationStart()
+                    disappearPictureOfTheDay()
+                    viewModel.sendServerRequest(takeDate(-2), onError = ::loadingError)
+                    binding.dataTextview.text = "Дата картинки " + takeDate(-2)
+                    Thread {
+                        Thread.sleep(durationSet)
+                        runOnUiThread {
+                            binding.TextviewConstraintSet.text = takeDate(-2)
+                        }
+                    }.start()
                 }
             }
         }
     }
 
-        private fun takeDate(count: Int): String {
-            val currentDate = Calendar.getInstance()
-            currentDate.add(Calendar.DAY_OF_MONTH, count)
-            val format1 = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-            format1.timeZone = TimeZone.getTimeZone("EST")
-            return format1.format(currentDate.time)
-        }
+    private fun takeDate(count: Int): String {
+        val currentDate = Calendar.getInstance()
+        currentDate.add(Calendar.DAY_OF_MONTH, count)
+        val format1 = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        format1.timeZone = TimeZone.getTimeZone("EST")
+        return format1.format(currentDate.time)
+    }
 
 
     private fun loadingError(throwable: Throwable) {
 
         Toast.makeText(mContext, "не прошла загрузка $throwable", Toast.LENGTH_LONG).show()
         binding.progressBar.visibility = ProgressBar.INVISIBLE;
-        animationFadePictureOaTheDay()
+
+        animationFadePictureOfTheDay()
+        binding.TextviewConstraintSet.text = selectedDate
+        constraintSetAnimationEnd()
     }
 
     private fun clickFab() {
@@ -167,42 +200,84 @@ class PictureOfTheDayFragment : Fragment() {
             is PictureOfTheDayState.Error -> {
                 Toast.makeText(mContext, "не прошла загрузка", Toast.LENGTH_LONG).show()
                 binding.progressBar.visibility = ProgressBar.INVISIBLE;
-                animationFadePictureOaTheDay()
+                animationFadePictureOfTheDay()
+                constraintSetAnimationEnd()
             }
             is PictureOfTheDayState.Loading -> {
-                binding.progressBar.visibility = ProgressBar.VISIBLE;
+//                binding.progressBar.visibility = ProgressBar.VISIBLE;
 
             }
             is PictureOfTheDayState.Success -> {
                 binding.imageView.load(pictureOfTheDayState.serverResponseData.hdurl)
 
-                animationFadePictureOaTheDay()
+                animationFadePictureOfTheDay()
+                constraintSetAnimationEnd()
 
                 binding.included.bottomSheetDescriptionHeader
                     .text = pictureOfTheDayState.serverResponseData.title
                 binding.included.bottomSheetDescription.text = pictureOfTheDayState
                     .serverResponseData.explanation
-                binding.progressBar.visibility = ProgressBar.INVISIBLE;
+                binding.progressBar.visibility = ProgressBar.INVISIBLE;// не пойму почему не отрабатывает код
             }
         }
 
     }
-    private fun animationFadePictureOaTheDay(){
-        val transition = TransitionSet()
-        val fade = Fade()
-        fade.duration = 5000
-        transition.addTransition(fade)
-        TransitionManager.beginDelayedTransition(binding.transitionsContainer, transition)
-        binding.imageView.visibility =  View.VISIBLE
+
+    private fun constraintSetAnimationEnd() {
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(binding.constraintContainer)
+        val changeBounds = ChangeBounds()
+        changeBounds.interpolator = AnticipateOvershootInterpolator(2.0f)
+        changeBounds.duration = durationSet
+        TransitionManager.beginDelayedTransition(binding.constraintContainer, changeBounds)
+
+            constraintSet.connect(R.id.TextviewConstraintSet, ConstraintSet.BOTTOM, R.id.main,
+                ConstraintSet.BOTTOM)
+            constraintSet.connect(R.id.TextviewConstraintSet, ConstraintSet.TOP, R.id.main,
+                ConstraintSet.TOP)
+            constraintSet.connect(R.id.TextviewConstraintSet, ConstraintSet.END, R.id.main,
+                ConstraintSet.END)
+        constraintSet.connect(R.id.TextviewConstraintSet, ConstraintSet.START, R.id.main,
+            ConstraintSet.START)
+            constraintSet.applyTo(binding.constraintContainer)
+
+        }
+
+
+    private fun constraintSetAnimationStart() {
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(binding.constraintContainer)
+        val changeBounds = ChangeBounds()
+
+        changeBounds.duration = durationSet/2
+        TransitionManager.beginDelayedTransition(binding.constraintContainer, changeBounds)
+        constraintSet.connect(R.id.TextviewConstraintSet, ConstraintSet.BOTTOM, R.id.main,
+            ConstraintSet.TOP)
+        constraintSet.connect(R.id.TextviewConstraintSet, ConstraintSet.END, R.id.main,
+            ConstraintSet.END)
+        constraintSet.connect(R.id.TextviewConstraintSet, ConstraintSet.START, R.id.main,
+            ConstraintSet.START)
+//        constraintSet.setVerticalBias(R.id.TextviewConstraintSet,0.02f)
+        constraintSet.applyTo(binding.constraintContainer)
     }
 
-    private fun disappearPictureOaTheDay(){
+
+    private fun animationFadePictureOfTheDay() {
+        val transition = TransitionSet()
+        val fade = Fade()
+        fade.duration = 4000
+        transition.addTransition(fade)
+        TransitionManager.beginDelayedTransition(binding.transitionsContainer, transition)
+        binding.imageView.visibility = View.VISIBLE
+    }
+
+    private fun disappearPictureOfTheDay() {
         val transition = TransitionSet()
         val fade = Fade()
         fade.duration = 2000
         transition.addTransition(fade)
         TransitionManager.beginDelayedTransition(binding.transitionsContainer, transition)
-        binding.imageView.visibility =  View.INVISIBLE
+        binding.imageView.visibility = View.INVISIBLE
     }
 
     private fun initBottomSheetBehavior() {
